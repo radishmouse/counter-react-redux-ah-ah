bb8bfd0ae7bb78d4ed0f41f5bbb232f5d5f25b99
Chris Aquino <chris.aquino@gmail.com>
2017-09-15 15:45:24 -0400

    whitespace

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -3,7 +3,9 @@ import { connect } from 'react-redux';
 import App from './App';
 
 const mapStateToProps = state => ({
-  values: Object.keys(state).reduce( (tally, key)  => tally + String(state[key]), '')
+  values: Object
+            .keys(state)
+            .reduce( (tally, key)  => tally + String(state[key]), '')
 });
 
 export default connect(mapStateToProps)(App);
